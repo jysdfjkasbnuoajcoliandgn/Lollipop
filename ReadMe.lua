b44611ae0742fd8db4217a44ce6b71ceb632ea18
Lala <80499479+lJustWantFame@users.noreply.github.com>
2022-02-14 01:02:44 -0500

    Update ReadMe.lua

--- ReadMe.lua
+++ ReadMe.lua
@@ -5,9 +5,7 @@
 # Anyways, this is just a simple nitro generator that will generate random nitro codes and also check them as they generate.
 # Once you download this and extract "Nitro Generator.rar"
 # You will have all the .txt files and the Nitro Generator application.
-# Once you run the application windows warning will popup saying that the .exe is a unknown application, (if you don't have this notification then great!) that is because this app is still new and has not been run in the community much at all since it has been private. The older the application becomes, the windowns warning will go away. So do not fear! Youre pc is safe yet another day!
-# Just click the "More info" button and click run anyway.
-# A console will appear and it should say "Starting"
+# Once you run the application, a console will appear and it should say "Starting"
 # Afterwards simply type ".generate".
 # All generated codes will go to the generated codes.txt file, Unknown/Used codes will go to the respected file, and Valid/Unused codes will go to the Valid Codes.txt file.
 # You will have to redeem them your self simply by copy and pasting the Valid codes, simply because i do not want my application logging into peoples discord accounts.
